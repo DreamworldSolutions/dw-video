335188c2072521e0263923d503aaf57cfbaec800
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2023-02-15 14:56:30 +0530

    feat: show default thumbnail of video. (#7)
    card link: https://a.kerika.net/C7r/board/Xr7q/Zy52

--- dw-video.js
+++ dw-video.js
@@ -1,28 +1,49 @@
 import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
-import { isElementAlreadyRegistered } from "@dreamworld/pwa-helpers/utils.js";
 import Player from '@vimeo/player';
+import dwFetch from '@dreamworld/fetch'
+
+// Components
+import './dw-loader.js';
 
 /**
- * A WebComponent to show a video on documentation & blog sites.
+ * A WebComponent to show a video thumbnail on documentation & blog sites.
  *
  * ## Behaviours
- * - Currntly support only [viemo](https://vimeo.com/) video.
+ * - Currntly support only [Vimeo](https://vimeo.com/) video.
  * - Auto compute height based on width css style.
- * ## Examples 
- *  ```html
- *    <dw-video
- *      src='https://player.vimeo.com/video/313303279'>
- *    </dw-video>
- *  ```
+ * - On thumbnail click, open [Vimeo](https://vimeo.com/) video in new tab.
+ * - If you want to show [Vimeo](https://vimeo.com/) actual video instead of a thumbnail then set the `inline` property as a `true`.
  *
- *  ```css
- *    dw-video {
- *      width: 500px;
- *    }
- *  ```
- * 
- * @fires video-loaded - when video is successfully loaded.
- * 
+ * ## Examples
+ *  - Default Examples
+ *    ```html
+ *      <dw-video
+ *        src='https://player.vimeo.com/video/313303279'>
+ *      </dw-video>
+ *    ```
+ *
+ *    ```css
+ *      dw-video {
+ *        width: 500px;
+ *      }
+ *    ```
+ *
+ * - Inline Video
+ *    ```html
+ *      <dw-video
+ *        inline
+ *        src='https://player.vimeo.com/video/313303279'>
+ *      </dw-video>
+ *    ```
+ *
+ *    ```css
+ *      dw-video {
+ *        width: 500px;
+ *      }
+ *    ```
+ *
+ * @fires loaded - when video thumbnail/inline-video is successfully loaded.
+ *
  * @element dw-video
  */
 export class DwVideo extends LitElement {
@@ -30,11 +51,12 @@ export class DwVideo extends LitElement {
     return [
       css`
         :host {
+          position: relative;
           display: block;
           width: 100%;
         }
 
-        #video-player {
+        #video-player, #img-container {
           overflow:hidden;
           padding-bottom:56.25%;
           position:relative;
@@ -42,12 +64,28 @@ export class DwVideo extends LitElement {
           border:var(--dw-video-border, none);
         }
 
-        #video-player iframe {
+        #video-player iframe, #img-container img {
           left:0;
           top:0;
           height:100%;
           width:100%;
           position:absolute;
+        }
+
+        #img-container img {
+          opacity: 0;
+          transition: opacity 0.3s ease-in-out;
+        }
+
+        :host([loaded]) #img-container img {
+          opacity: 1;
+        }
+
+        dw-loader {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%)
         }
       `,
     ];
@@ -62,30 +100,78 @@ export class DwVideo extends LitElement {
       src: {
         type: String
       },
+
+      /**
+       * If `true` then shows a inline vimeo video, Otherwise shows a viemo video thumbnail.
+       */
+      inline: {
+        type: Boolean,
+        reflect: true
+      },
+
+      /**
+       * Video thumbnail-url.
+       */
+      _thumbnailURL: {
+        type: String
+      },
+
+      /**
+       * `true` when id content is loaded.
+       */
+      _previewLoaded: { type: Boolean, reflect: true, attribute: 'loaded'}
     };
   }
 
   constructor() {
     super();
     this.doNotDelayRendering = true;
+    this._previewLoaded = false;
   }
 
   render() {
     return html`
-      <div id="video-player"></div>
+      ${this.inline ? html`
+        <div id="video-player"></div>
+      `: html`
+        <div id="img-container">
+          <a href=${this.src} target="_blank">
+            <img @load=${this.__onPreviewLoad} src=${this._thumbnailURL}/>
+          </a>
+        </div>
+      `}
+
+      ${!this._previewLoaded ? html`<dw-loader></dw-loader>` : ''}
     `;
   }
 
-  __onVideoLoad() {
-    this.dispatchEvent(new CustomEvent('video-loaded', { detail: { } }, { bubbles: false }));
+  __onPreviewLoad() {
+    this._previewLoaded = true;
+    this.dispatchEvent(new CustomEvent('loaded', { detail: { } }, { bubbles: false }));
   }
 
   updated(changeProps) {
     super.updated && super.updated(changeProps);
-    if(changeProps.has('src')) {
+    if(changeProps.has('src') || changeProps.has('inline')) {
+      this._previewLoaded = false;
       if(this.src) {
-        this.__loadVideo();
+        if(this.inline) {
+          this.__loadVideo();
+        } else {
+          this.__loadVideoThumbnail();
+        }
       }
+    }
+  }
+
+  async __loadVideoThumbnail() {
+    try {
+      const response = await dwFetch(`https://vimeo.com/api/oembed.json?url=${this.src}`);
+      let responseText; try { responseText = await response.text(); responseText = responseText.trim(); } catch (err) {}
+      let responseJSON; try { responseJSON = JSON.parse(responseText); } catch (e) {}
+      this._thumbnailURL = responseJSON && (responseJSON['thumbnail_url_with_play_button'] || responseJSON['thumbnail_url']) || '';
+    } catch (error) {
+      console.error("dw-video: load video thumbnail failed, due to this: ", console.error());
     }
   }
 
@@ -103,7 +189,7 @@ export class DwVideo extends LitElement {
     const el = this.shadowRoot.querySelector('#video-player');
     this._player = new Player(el, options);
     await this._player.ready();
-    this.__onVideoLoad();
+    this.__onPreviewLoad();
     this._playVideo();
   }
 
@@ -113,9 +199,5 @@ export class DwVideo extends LitElement {
     } catch (error) {}
   }
 }
-  
-if (isElementAlreadyRegistered("dw-video")) {
-  console.warn("lit: 'dw-video' is already registered, so registration skipped.");
-} else {
-  customElements.define("dw-video", DwVideo);
-}
+
+customElements.define("dw-video", DwVideo);
